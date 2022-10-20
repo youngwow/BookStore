82b8efb09154974f12682a22d31358a80eb8d886
youngwow <nikitaboyarkin15@gmail.com>
2022-10-20 12:57:44 +0300

    Search: done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true
@@ -22,6 +23,7 @@ db.once('open', () => console.log('Connected to Mongoose.'));
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authorsRouter = require('./routes/authors');
 
 const app = express();
 
@@ -32,6 +34,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,22 +43,23 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/authors', authorsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(process.env.PORT || 3000);
 
